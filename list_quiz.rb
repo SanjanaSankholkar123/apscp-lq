800928c7b77e27615a43d43cee66ab2858c74a8d
Sanjana Sankholkar <sanjan.sankholkar@gmail.com>
2019-10-08 09:16:40 -0600

    Solve series_up function by using if then statements

--- list_quiz.rb
+++ list_quiz.rb
@@ -31,6 +31,40 @@ def bigger_two(list1, list2)
   end
 end
 
-puts bigger_two([1, 2], [3, 4]) # [3, 4]
-puts bigger_two([1, 7], [4, 4]) # [1, 7]
-puts bigger_two([5, 6], [2, 2]) # [5, 6]
+# puts bigger_two([1, 2], [3, 4]) # [3, 4]
+# puts bigger_two([1, 7], [4, 4]) # [1, 7]
+# puts bigger_two([5, 6], [2, 2]) # [5, 6]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# series_up
+def series_up(n)
+  array = []
+  g1 = [1]
+  g2 = [1, 2]
+  g3 = [1, 2, 3]
+  g4 = []
+  if n == 1
+    return "#{g1}" # Looking back at my bigger_two code, I realized I was being stupid so I decided not to do that for these arrays
+  end
+  if n == 2
+    pattern = g1 + g2
+    array.push(pattern)
+    return "#{array}"
+  end
+  if n == 3
+    pattern = g1 + g2 + g3
+    array.push(pattern)
+    return "#{array}"
+  end
+  if n > 3
+    g4.push(n)
+    pattern = g1 + g2 + ((g3)*(n - 2)) + g4
+    array.push(pattern)
+    return "#{array}"
+  end
+end
+
+puts series_up(1) # [1]
+puts series_up(2) # [1, 1, 2]
+puts series_up(3) # [1, 1, 2, 1, 2, 3]
+puts series_up(4) # [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
