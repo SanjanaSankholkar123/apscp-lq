d339fa1a45fb684a0bcdebf81d3a02713b6ba93e
Sanjana Sankholkar <sanjan.sankholkar@gmail.com>
2019-10-08 08:52:29 -0600

    Solve bigger_two by using if then statements

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,8 +13,24 @@ def three_even?(list)
   return false
 end
 
-puts three_even?([2, 1, 3, 5]) # false
-puts three_even?([2, 4, 12, 5]) # true
-puts three_even?([2, 1, 4, 6]) # false
-puts three_even?([1, 4, 6, 4]) # true
-puts three_even?([]) # false
+# puts three_even?([2, 1, 3, 5]) # false
+# puts three_even?([2, 4, 12, 5]) # true
+# puts three_even?([2, 1, 4, 6]) # false
+# puts three_even?([1, 4, 6, 4]) # true
+# puts three_even?([]) # false
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# bigger_two
+def bigger_two(list1, list2)
+  if (list1[0] + list1[1]) > (list2[0] + list2[1])
+    return "[#{list1[0]}, #{list1[1]}]" # I solved it like this because otherwise, it would just print out each number individually
+  elsif (list1[0] + list1[1]) < (list2[0] + list2[1])
+    return "[#{list2[0]}, #{list2[1]}]"
+  elsif (list1[0] + list1[1]) == (list2[0] + list2[1])
+    return "[#{list1[0]}, #{list1[1]}]"
+  end
+end
+
+puts bigger_two([1, 2], [3, 4]) # [3, 4]
+puts bigger_two([1, 7], [4, 4]) # [1, 7]
+puts bigger_two([5, 6], [2, 2]) # [5, 6]
